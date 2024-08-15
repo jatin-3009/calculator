b34f36f5a6e7c57ed14b3cd67bd747c94dd07313
jatin-3009 <jatinninawe02@gmail.com>
2024-08-15 23:31:06 +0530

    initial commit

---
+++ src/test/java/com/AppTest.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class AppTest {
+    @Test void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
