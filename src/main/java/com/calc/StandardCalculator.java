46d33bd5b50e294a55be3c0ee4919b5a5d5810ea
jatin-3009 <jatinninawe02@gmail.com>
2024-08-18 12:31:59 +0530

    Added scientific calculator class, defined advanced math methods, wrote test cases

--- src/main/java/com/calc/StandardCalculator.java
+++ src/main/java/com/calc/StandardCalculator.java
@@ -1,34 +1,32 @@
 package com.calc;
 
 public class StandardCalculator {
-    private double result;
+    protected double result;
 
     public static void getVersion(){
         System.out.println("Standard Calculator 1.0");
     }
 
     // For two ints
-    public void add(int num1, int num2) {
+    public final void add(int num1, int num2) {
         add((double) num1, (double) num2);
     }
 
-    public void subtract(int num1, int num2) {
+    public final void subtract(int num1, int num2) {
         subtract((double) num1, (double) num2);
     }
 
-    public void multiply(int num1, int num2) {
+    public final void multiply(int num1, int num2) {
         multiply((double) num1, (double) num2);
     }
 
-    public void divide(int num1, int num2) {
+    public final void divide(int num1, int num2) {
         divide((double) num1, (double) num2);
     }
 
     // For two doubles
-    public void add(double num1, double num2) {
+    public final void add(double num1, double num2) {
         double result = num1 + num2;
-
-        System.out.println("result: " + result);
 
         if (result == Double.MAX_VALUE || result == Double.POSITIVE_INFINITY) {
             throw new ArithmeticException("Double overflow");
@@ -37,7 +35,7 @@ public class StandardCalculator {
         this.result = result;
     }
 
-    public void subtract(double num1, double num2) {
+    public final void subtract(double num1, double num2) {
         double result = num1 - num2;
 
         if (result == -Double.MAX_VALUE || result == Double.NEGATIVE_INFINITY) {
@@ -47,11 +45,11 @@ public class StandardCalculator {
         this.result = result;
     }
 
-    public void multiply(double num1, double num2) {
+    public final void multiply(double num1, double num2) {
         result = num1 * num2;
     }
 
-    public void divide(double num1, double num2) {
+    public final void divide(double num1, double num2) {
         result = num1 / num2;
     }
 
---
+++ src/test/java/com/calc/ScientificCalculatorTest.java
@@ -0,0 +1,78 @@
+package com.calc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ScientificCalculatorTest {
+    private ScientificCalculator scientificCalculator;
+
+    @BeforeEach
+    void setup() {
+        scientificCalculator = new ScientificCalculator();
+    }
+
+    @Test
+    @DisplayName("Test Sin of given Double")
+    void testSinOperation() {
+        double expectedResult = 1.0;
+        scientificCalculator.sin(Math.PI / 2);
+        double actualResult = scientificCalculator.getResult();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test Cos of given Double")
+    void testCosOperation() {
+        double expectedResult = -1.0;
+        scientificCalculator.cos(Math.PI);
+        double actualResult = scientificCalculator.getResult();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test Tan of given Double")
+    void testTanOperation() {
+        double expectedResult = 0;
+        scientificCalculator.tan(0);
+        double actualResult = scientificCalculator.getResult();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test Log of given Double")
+    void testLogOperation() {
+        double expectedResult = 1;
+        scientificCalculator.log(Math.exp(1));
+        double actualResult = scientificCalculator.getResult();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test Square Root of given Double")
+    void testSquareRootOperation() {
+        double expectedResult = 2;
+        scientificCalculator.sqrt(4);
+        double actualResult = scientificCalculator.getResult();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test Cube Root of given Double")
+    void testCubeRootOperation() {
+        double expectedResult = 2;
+        scientificCalculator.cbrt(8);
+        double actualResult = scientificCalculator.getResult();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test Square of given Double")
+    void testSquareOperation() {
+        double expectedResult = 16;
+        scientificCalculator.square(4);
+        double actualResult = scientificCalculator.getResult();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+}
